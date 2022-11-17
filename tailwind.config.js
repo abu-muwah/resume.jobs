a6f7765ff1901d1db4c0065cf9b4b2094d776540
ABU_MUWAH <abu.muwah@gmail.com>
2022-11-17 11:08:28 +0430

    Customizing container

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    container: { 
+      center: true,
+      screens: {
+        'xl': '1120px',
+      },
+    },
     extend: {},
   },
   plugins: [],
